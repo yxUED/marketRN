08630564fc24e67394fa4e246dcfdc26d0773de3
王迪 <wangdi05@yxqiche.com>
2018-06-13 14:07:36 +0800

    tabbar icon

--- src/containers/routes/tabRoutes.js
+++ src/containers/routes/tabRoutes.js
@@ -10,6 +10,10 @@ import Account from '../tabs/Account';
 import homeIcon from '../../img/tabbar/toolbar-home-icon-normal.png';
 import marketIcon from '../../img/tabbar/toolbar-study-icon-normal.png';
 import mineIcon from '../../img/tabbar/toolbar-mine-icon-normal.png';
+import homeIconPress from '../../img/tabbar/toolbar-home-icon-press.png';
+import marketIconPress from '../../img/tabbar/toolbar-study-icon-press.png';
+import mineIconPress from '../../img/tabbar/toolbar-mine-icon-press.png';
+
 
 const Tabs = createBottomTabNavigator({
   Home: Home,
@@ -20,18 +24,21 @@ const Tabs = createBottomTabNavigator({
   swipeEnabled: false,
   animationEnabled: false,
 	navigationOptions:({navigation}) => {
-		let tabIcon, labelName;
+		let tabIcon, labelName, tabIconPress;
 		switch (navigation.state.routeName) {
 			case 'Home':
 				tabIcon = homeIcon;
+				tabIconPress = homeIconPress;
 				labelName = '首页';
 				break;
 			case 'Market':
 				tabIcon = marketIcon;
+				tabIconPress = marketIconPress;
 				labelName = '长时拍';
 				break;
 			case 'Account':
 				tabIcon = mineIcon;
+				tabIconPress = mineIconPress;
 				labelName = '我的';
 				break;
 			default:
@@ -40,7 +47,7 @@ const Tabs = createBottomTabNavigator({
 			tabBarIcon:(focused, tintColor)=>{
 				return (
 					<Image
-						source={tabIcon}
+						source={ focused ? tabIconPress : tabIcon }
 						style={[{ width: 26, height: 26 }, { tintColor }]}
 					/>
 				);
@@ -49,7 +56,7 @@ const Tabs = createBottomTabNavigator({
 		}
 	},
   tabBarOptions: {
-    activeTintColor: 'blue',
+    activeTintColor: '#149DFF',
     showIcon: true,
     // android 中TabBar下面会显示一条线，高度设为 0 后就不显示线了
     indicatorStyle: { height: 0 },
@@ -57,7 +64,7 @@ const Tabs = createBottomTabNavigator({
       fontSize: 12
     },
     style: {
-      backgroundColor: '#2784E2',
+      backgroundColor: '#ffffff',
       height: 60
     },
   }
